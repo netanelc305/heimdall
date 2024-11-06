cd5d120c9b5555e894b8ff52f2a5489bc135d07f
netanelc305 <netanelc305@protonmail.com>
2024-11-06 20:44:12 +0200

    Add support for Linux process and filesystem operations
    Introduce `LinuxProcess`, `LinuxProcesses`, and `LinuxFileSystem` classes to handle Linux process attributes and filesystem interactions. Refactor `create_heimdall_client` to support creating a `LinuxClient` for managing virtual machines running Linux.

--- heimdall/create_client.py
+++ heimdall/create_client.py
@@ -4,11 +4,12 @@ from libvmi import Libvmi
 
 from heimdall.core.isf_file import ISFFile, OSType
 from heimdall.heimdall_client import HeimdallClient
+from heimdall.os_related.linux.linux_client import LinuxClient
 from heimdall.os_related.macos.macos_client import MacOSClient
 
 
 def create_heimdall_client(vmi: Libvmi, profile: Path) -> HeimdallClient:
     """ Factory function to create a HeimdallClient for managing virtual machines. """
     isf_file = ISFFile(profile)
-    clients = {OSType.MACOS: MacOSClient}
+    clients = {OSType.MACOS: MacOSClient, OSType.LINUX: LinuxClient}
     return clients.get(isf_file.get_os_type(), HeimdallClient)(vmi, profile)
